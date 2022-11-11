08a1dd8502a37191b1d6d3cf3f1a4feef21be6a5
Chlunidia Putri Kieswendha <chlunidia.putri.ft21@mail.umy.ac.id>
2022-11-11 17:11:59 +0700

    Add project files.

---
+++ singly_linked_list/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace singly_linked_list
+{
+    // each node consist of the information part and link to the next mode
+    class Node
+    {
+        public int rollNumber;
+        public string name;
+        public Node next;
+    }
+}
